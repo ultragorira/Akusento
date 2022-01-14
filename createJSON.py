0d9edf74f86e30ad873af8e2a450abd5206cb214
ultragorira <lorisdeluca1988@gmail.com>
2022-01-14 11:25:28 +0200

    JSON creation

--- createJSON.py
+++ createJSON.py
@@ -17,74 +17,70 @@ def create_JSONS(full_data):
   fileCount = 0
 
   # Iterate through each row in worksheet and fetch values into dict
-  for rownum in range(2, len(full_data)):
-      data = {}
-      data['sessionLocalStartTime'] = full_data.cell(rownum, 1).value  
-      data['audioFileName'] = full_data.cell(rownum, 2).value 
-      data['collectionId'] = full_data.cell(rownum, 3).value 
-      data['collectionUuid'] = full_data.cell(rownum,4).value 
-      data['audioFormat'] = {}
-      if (full_data.cell(rownum, 5).value == 'Little'):
-          endian = True
-      else:
-          endian = False 
-      data['audioFormat']['bigEndian'] = endian
-      data['audioFormat']['channels'] = full_data.cell(rownum, 6).value
-      data['audioFormat']['encoding'] = full_data.cell(rownum, 7).value
-      data['audioFormat']['frameRate'] = full_data.cell(rownum, 8).value
-      data['audioFormat']['frameSize'] = full_data.cell(rownum, 9).value
-      data['audioFormat']['sampleRate'] = full_data.cell(rownum, 10).value
-      data['audioFormat']['sampleSizeInBits'] = full_data.cell(rownum, 11).value
-      data['audioFormat']['audioDurationMillis'] = full_data.cell(rownum, 12).value
-      data['audioFormat']['format'] = full_data.cell(rownum, 13).value
-      data['audioFormat']['sourceType'] = full_data.cell(rownum, 14).value
-      data['sessionInfo'] = {}
-      data['sessionInfo']['languageCollected'] = full_data.cell(rownum, 15).value
-      data['sessionInfo']['ageRange'] = full_data.cell(rownum, 16).value
-      data['sessionInfo']['gender'] = full_data.cell(rownum, 17).value
-      data['sessionInfo']['countryLived'] = full_data.cell(rownum, 18).value
-      data['sessionInfo']['languageSpokenDaily'] = full_data.cell(rownum, 19).value
-      data['sessionInfo']['primaryMicrophone'] = full_data.cell(rownum, 20).value
-      data['transcription'] = {}
-      data['transcription']['audioDurationMillis'] = full_data.cell(rownum, 21).value
-      data['transcription']['domain'] = full_data.cell(rownum, 22).value
-      data['transcription']['id'] = str(full_data.cell(rownum, 23).value)
-      data['transcription']['phrase'] = full_data.cell(rownum, 24).value
-      data['transcription']['startEndpoint'] = full_data.cell(rownum, 25).value
-      data['transcription']['stopEndpoint'] = full_data.cell(rownum, 26).value
-      data['transcription']['type'] = full_data.cell(rownum, 27).value
-      data['transcription']['qualityChecks'] = {}
-      data['transcription']['wakeword'] = full_data.cell(rownum, 29).value
-      data['transcription']['intent'] = full_data.cell(rownum,30).value
-      data['additionalMetadata'] = {}
-      data['additionalMetadata']['annotation'] = {}
-      data['additionalMetadata']['phoneModel'] = full_data.cell(rownum, 32).value
-      data['additionalMetadata']['phoneBrand'] = full_data.cell(rownum, 33).value
-      data['additionalMetadata']['speakerId'] = full_data.cell(rownum, 34).value
-      data['additionalMetadata']['backgroundNoiseLevelInDb'] = str(full_data.cell(rownum, 35).value)
-      data['additionalMetadata']['operatingSystem'] = full_data.cell(rownum, 36).value
-      data['additionalMetadata']['backgroundNoise'] = full_data.cell(rownum, 37).value
-      data['additionalMetadata']['speechImpediment'] = full_data.cell(rownum, 38).value
-      data['additionalMetadata']['dialect'] = full_data.cell(rownum,39).value
-      if (full_data.cell(rownum,40).value == 'Chicago'):
-        region = 'Chicago'
-      elif (full_data.cell(rownum,40).value == 'North East'):
-        region = 'Northeast'
-      elif ((full_data.cell(rownum,40).value).split('-')[0] == 'South'):
-        region = 'South'
-      else:
-        region = 'Other'
-      data['additionalMetadata']['region'] = full_data.cell(rownum,40).value
-      data['additionalMetadata']['other_city_state'] = full_data.cell(rownum,41).value
+  for rownum in range(len(full_data.index)):
+    data = {}
+    data['sessionLocalStartTime'] = int(full_data['sessionLocalStartTime'][rownum+1]) 
+    data['audioFileName'] = full_data['audioFileName'][rownum+1]
+    data['collectionId'] = full_data['collectionId'][rownum+1]
+    data['collectionUuid'] = full_data['collectionUuid'][rownum+1] 
+    data['audioFormat'] = {}
+    if (full_data['bigEndian'][rownum+1] == 'Little'):
+      endian = "true"
+    else:
+      endian = "false" 
+    data['audioFormat']['bigEndian'] = endian
+    data['audioFormat']['channels'] = int(full_data['channels'][rownum+1])
+    data['audioFormat']['encoding'] = full_data['encoding'][rownum+1]
+    data['audioFormat']['frameRate'] = int(full_data['frameRate'][rownum+1])
+    data['audioFormat']['frameSize'] = int(full_data['frameSize'][rownum+1])
+    data['audioFormat']['sampleRate'] = int(full_data['sampleRate'][rownum+1])
+    data['audioFormat']['sampleSizeInBits'] = int(full_data['sampleSizeInBits'][rownum+1])
+    data['audioFormat']['audioDurationMillis'] = int(full_data['audioDurationMillis'][rownum+1])
+    data['audioFormat']['format'] = full_data['format'][rownum+1]
+    data['audioFormat']['sourceType'] = full_data['sourceType'][rownum+1]
+    data['sessionInfo'] = {}
+    data['sessionInfo']['languageCollected'] = full_data['languageCollected'][rownum+1]
+    data['sessionInfo']['ageRange'] = full_data['ageRange'][rownum+1]
+    data['sessionInfo']['gender'] = full_data['gender'][rownum+1]
+    data['sessionInfo']['countryLived'] = full_data['countryLived'][rownum+1]
+    data['sessionInfo']['languageSpokenDaily'] = full_data['languageSpokenDaily'][rownum+1]
+    data['sessionInfo']['primaryMicrophone'] = full_data['primaryMicrophone'][rownum+1]
+    data['transcription'] = {}
+    data['transcription']['domain'] = full_data['domain'][rownum+1]
+    data['transcription']['id'] = str(full_data['id'][rownum+1])
+    data['transcription']['phrase'] = full_data['phrase'][rownum+1]
+    data['transcription']['annotation'] = full_data['annotation'][rownum+1]
+    data['transcription']['startEndpoint'] = full_data['startEndpoint'][rownum+1]
+    data['transcription']['stopEndpoint'] = full_data['stopEndpoint'][rownum+1]
+    data['transcription']['type'] = full_data['type'][rownum+1]
+    data['transcription']['qualityChecks'] = {}
+    data['transcription']['wakeword'] = full_data['wakeword'][rownum+1]
+    data['transcription']['intent'] = full_data['intent'][rownum+1]
+    data['additionalMetadata'] = {}
+    data['additionalMetadata']['phoneModel'] = full_data['phoneModel'][rownum+1]
+    data['additionalMetadata']['phoneBrand'] = full_data['phoneBrand'][rownum+1]
+    data['additionalMetadata']['speakerId'] = full_data['speakerId'][rownum+1]
+    data['additionalMetadata']['backgroundNoiseLevelInDb'] = str(full_data['backgroundNoiseLevelInDb'][rownum+1])
+    data['additionalMetadata']['distance'] = full_data['distance'][rownum+1]
+    data['additionalMetadata']['backgroundCondition'] = full_data['backgroundCondition'][rownum+1]
+    data['additionalMetadata']['operatingSystem'] = full_data['operatingSystem'][rownum+1]
+    data['additionalMetadata']['speechType'] = str(full_data['speechType'][rownum+1]).rstrip()
+    data['additionalMetadata']['accentLevel'] = full_data['accentLevel'][rownum+1]
+    data['additionalMetadata']['surveyQuestion1'] = full_data['surveyQuestion1'][rownum+1]
+    data['additionalMetadata']['surveyQuestion2'] = full_data['surveyQuestion2'][rownum+1]
+    data['additionalMetadata']['surveyQuestion3'] = full_data['surveyQuestion3'][rownum+1]
+    data['additionalMetadata']['surveyQuestion4'] = full_data['surveyQuestion4'][rownum+1]
+    data['additionalMetadata']['surveyQuestion4a'] = full_data['surveyQuestion4a'][rownum+1]
+    data['additionalMetadata']['surveyQuestion5'] = full_data['surveyQuestion5'][rownum+1]
 
-      if data['sessionLocalStartTime'] == "None" or data['sessionLocalStartTime'] == "" or data['sessionLocalStartTime'] == None:
-        break
-      j = json.dumps(data, indent=4, ensure_ascii=False).encode('utf8')
-      # Write to file
-      with open(os.path.join(JSON_Folder, data['audioFileName'].replace('wav','') + 'json'), 'wb') as f:
-        f.write(j)
-        json_list.clear()
-      fileCount += 1
+    if data['sessionLocalStartTime'] == "None" or data['sessionLocalStartTime'] == "" or data['sessionLocalStartTime'] == None:
+      break
+    j = json.dumps(data, indent=4, ensure_ascii=False).encode('utf8')
+    # Write to file
+    with open(os.path.join(JSON_Folder, data['audioFileName'].replace('wav','') + 'json'), 'wb') as f:
+      f.write(j)
+      json_list.clear()
+    fileCount += 1
   print("{} JSON files have been generated!".format(fileCount))
   os.startfile(JSON_Folder)
 
@@ -98,10 +94,10 @@ def read_data_from_gspread():
   client = gspread.authorize(creds)
   sheet = client.open('TECH_ACCENTS').worksheet('JSON_DumpCollection')  # Open the spreadhseet
 
+  print('Getting Data from Gsheet ' + sheet.title) 
   df = pd.DataFrame(sheet.get_all_records())
   #Filtering the dataframe by only the ID interested in
   df_by_id = df.loc[df['audioFileName'].str.contains(str(id_participant), case=False)]
-  print(df_by_id)
   create_JSONS(df_by_id)
 
 if __name__ == '__main__':
