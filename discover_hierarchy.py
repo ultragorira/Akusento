51eeddc112b52952ceac04609d0d49fe96085a3b
ultragorira <lorisdeluca1988@gmail.com>
2022-04-25 16:34:00 +0300

    Bug fix

--- discover_hierarchy.py
+++ discover_hierarchy.py
@@ -1,21 +1,22 @@
 from googleapiclient.discovery import build
-from apiclient import errors
 from oauth2client.service_account import ServiceAccountCredentials
-from apiclient import errors
+import csv
 
 
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/drive']
 JSON_creds = 'Bot_Creds.json'
+HEADERS = ['FILENAME', 'FILE ID', 'NAME', 'EMAIL', 'ROLE']
+data = []
 
 def main():
     folderId = input('Give folder ID: ')
     creds = ServiceAccountCredentials.from_json_keyfile_name(JSON_creds, SCOPES)
     service = build('drive', 'v3', credentials=creds)
-    list_files = downloadFolder(service, folderId)
-    permissions_found = retrieve_permissions(service, list_files)
+    searchFolder(service, folderId)
+    write_report(data)
 
-def downloadFolder(service, fileId):
+def searchFolder(service, fileId):
 
     results = service.files().list(
         pageSize=300,
@@ -26,9 +27,15 @@ def downloadFolder(service, fileId):
         ).execute()
 
     files = results.get('files', [])
-    return(files)
 
-def retrieve_permissions(service, files):
+    for file in files:
+      if file['mimeType'] == 'application/vnd.google-apps.folder':
+        searchFolder(service, file['id']) #Recursively checking other subfolders
+      else:
+        #print(file['id'])
+        retrieve_permissions(service, file['id'], file['name'])
+
+def retrieve_permissions(service, file_id, file_name):
   """Retrieve a list of permissions.
 
   Args:
@@ -37,10 +44,28 @@ def retrieve_permissions(service, files):
   Returns:
     List of permissions.
   """
-  for file in files:
-    permissions = service.permissions().list(fileId=file['id'],
+  permissions = service.permissions().list(fileId=file_id,
         fields='*',
         supportsAllDrives=True).execute()
+  try: 
+    for permission in permissions['permissions']:
+      if 'youroppa' not in permission['displayName']: #This is the bot name
+        try:
+          data.append([file_name, file_id, permission['displayName'], permission['emailAddress'],permission['role']])
+        except Exception:
+          print(f'Could not retrieve info on {file_id}')
+      else:
+        continue
+  except Exception as e:
+    print(e)
+      #print(permission)  
 
-    print(permissions)  
+def write_report(data):
+
+    with open('Report.csv', mode='w', newline='', encoding='utf-8-sig') as f:
+        csv_writer = csv.writer(f)
+        csv_writer.writerow(HEADERS)
+        csv_writer.writerows(data)
+
+
 main()
